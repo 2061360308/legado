8e9398fa2f72688e76202a18b223c0cf09087b9c
2061360308 <2061360308@qq.com>
2024-05-24 23:13:50 +0800

    auto_reply测试

--- manage_tools/auto_reply.py
+++ manage_tools/auto_reply.py
@@ -1,7 +1,9 @@
+from github import Github
 import os
 import json
 
 # 获取环境变量中的事件数据文件路径
+token = os.getenv('GITHUB_TOKEN')
 event_path = os.getenv('GITHUB_EVENT_PATH')
 
 # 读取事件数据文件
@@ -9,6 +11,16 @@ with open(event_path, 'r') as f:
     event_data = json.load(f)
 
 # 获取触发事件的讨论的详细信息
-discussion = event_data['discussion']
-print(discussion['title'])  # 打印讨论标题
-print(discussion['body'])  # 打印讨论内容
+discussion_number = event_data['discussion']['number']
+print(event_data['discussion'])  # 打印讨论的详细信息
+print(event_data['discussion']['title'])  # 打印讨论标题
+print(event_data['discussion']['body'])  # 打印讨论内容
+print(event_data['discussion']['number'])  # 打印讨论编号
+
+# 使用GitHub API获取讨论
+g = Github(token)
+repo = g.get_repo("2061360308/legado")  # 替换为你的仓库主名和仓库名
+discussion = repo.get_discussion(discussion_number)
+
+# 对讨论进行自动回复
+discussion.create_comment("你的回复内容")
